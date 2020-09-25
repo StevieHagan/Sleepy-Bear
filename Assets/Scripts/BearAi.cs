9b61ce82db648960e4564c1cc0d8b6175c92209a
Stephen Hagan <64257053+StevieHagan@users.noreply.github.com>
2020-09-25 22:06:38 +0100

    Improved Bear off-path movement. Separated AI from movement script.

---
+++ Assets/Scripts/BearAi.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearAi : MonoBehaviour
+{
+    [Tooltip("Vertical limit to waypoint detection, to stop Bear detecting waypoints directly above or below")]
+    [SerializeField] float verticalLimit = 3f;
+    [Tooltip("Sets the direct distance away from which Bear can detect waypoints")]
+    [SerializeField] float visionDistance = 10f;
+    [Tooltip("Sets the angle (either side of forward) within which Bear can detect the next waypoint while on a path")]
+    [SerializeField] float onPathVisionAngle = 90f;
+    [Tooltip("Sets the angle (either side of forward) within which Bear can detect the next waypoint while roaming loose")]
+    [SerializeField] float offPathVisionAngle = 30f;
+
+    Waypoint[] waypoints;
+
+    private void Start()
+    {
+        waypoints = FindObjectsOfType<Waypoint>();
+    }
+
+    public Waypoint GetNextWaypoint(Waypoint currentDestination, float arrivalDistance, bool isOnPath)
+    {
+        if (currentDestination == null || currentDestination.GetManualWaypoints() == null)
+        {
+            return AutoDetectNextWaypoint(arrivalDistance, isOnPath);
+        }
+        else
+        {
+            return SelectFromManualWaypoints(currentDestination);
+        }
+    }
+
+    private Waypoint AutoDetectNextWaypoint(float arrivalDistance, bool isOnPath)
+    {//finds the closest waypoint infront of Bear and returns it
+
+        //create a HashSet of waypoints which are within Bear's detection range
+        HashSet<Waypoint> detectibleWaypoints = new HashSet<Waypoint>();
+
+        foreach (Waypoint waypoint in waypoints)
+        {   //check vertical and total distnce first
+            if (Mathf.Abs(transform.position.y - waypoint.transform.position.y) < verticalLimit
+               && Vector3.Distance(transform.position, waypoint.transform.position) < visionDistance
+               && Vector3.Distance(transform.position, waypoint.transform.position) > arrivalDistance)
+            {
+                //create a direction vector between the Bear and the Waypoint
+                Vector3 waypointDir = waypoint.transform.position - transform.position;
+                //if the angle is less than required, add this Waypoint to the set
+                if (Vector3.Angle(transform.forward, waypointDir) < (isOnPath ? onPathVisionAngle : offPathVisionAngle))
+                {
+                    detectibleWaypoints.Add(waypoint);
+                }
+            }
+        }
+        if (detectibleWaypoints.Count == 0) //there are no waypoints in range
+        {
+            Debug.Log("There are no waypoints within detection range");
+            return null;
+        }
+        else if (detectibleWaypoints.Count == 1) //only one waypoint is in range, return it
+        {
+            foreach (Waypoint waypoint in detectibleWaypoints)
+            {
+                Debug.Log("There is one waypoint in range at position " + waypoint.transform.position);
+                return waypoint;
+            }
+            return null;
+        }
+        else //multiple waypoints, find which one is closest to Bear.
+        {
+            Waypoint closestWaypoint = null;
+            float closestDistance = 1000f;
+
+            foreach (Waypoint waypoint in detectibleWaypoints)
+            {
+                float distance = Vector3.Distance(transform.position, waypoint.transform.position);
+                if (distance < closestDistance)
+                {
+                    closestWaypoint = waypoint;
+                    closestDistance = distance;
+                }
+            }
+            Debug.Log("There are " + detectibleWaypoints.Count + " waypoints in range.");
+            return closestWaypoint;
+
+        }
+    }
+
+    private Waypoint SelectFromManualWaypoints(Waypoint currentDestination)
+    {//finds which of the manually set waypoints is closest to Bear's forward
+
+        Waypoint[] manualWaypoints = currentDestination.GetManualWaypoints();
+
+        if (manualWaypoints.Length == 0)
+        {
+            return null;
+        }
+        else if (manualWaypoints.Length == 1)
+        {
+            return manualWaypoints[0];
+        }
+        else
+        {
+            Waypoint forwardestWaypoint = null;
+            float narrowestAngle = 360f;
+
+            foreach (Waypoint waypoint in manualWaypoints)
+            {
+                Vector3 targetDir = waypoint.transform.position - transform.position;
+                float targetAngle = Vector3.Angle(targetDir, transform.forward);
+                if (targetAngle < narrowestAngle)
+                {
+                    forwardestWaypoint = waypoint;
+                    narrowestAngle = targetAngle;
+                }
+            }
+            return forwardestWaypoint;
+
+        }
+    }
+}
--- Assets/Scripts/BearMover.cs
+++ Assets/Scripts/BearMover.cs
@@ -7,16 +7,10 @@ public class BearMover : MonoBehaviour
 {
     [Tooltip("Sets the distance under which Bear will stop walking towards the current waypoint and look for the next one")]
     [SerializeField] float arrivalDistance = 0.5f;
-    [Tooltip("Sets the direct distance away from which Bear can detect waypoints")]
-    [SerializeField] float visionDistance = 10f;
-    [Tooltip("Vertical limit to waypoint detection, to stop Bear detecting waypoints directly above or below")]
-    [SerializeField] float verticalLimit = 3f;
-    [Tooltip("Sets the angle (either side of forward) within which Bear can detect the next waypoint")]
-    [SerializeField] float visionAngle = 90f;
     [Tooltip("READ ONLY - Indicates if Bear is currently on a path or roaming loose.")]
-    [SerializeField] bool isOnPath = true;
+    [SerializeField] bool isOnPath = false;
 
-    Waypoint[] waypoints;
+    BearAi ai;
     Waypoint currentDestination;
     NavMeshAgent navAgent;
     CapsuleCollider mainCollider;
@@ -25,11 +19,11 @@ public class BearMover : MonoBehaviour
 
     void Start()
     {
+        ai = GetComponent<BearAi>();
         mainCollider = GetComponent<CapsuleCollider>();
         navAgent = GetComponent<NavMeshAgent>();
-        waypoints = FindObjectsOfType<Waypoint>();
 
-        currentDestination = GetNextWaypoint();
+        currentDestination = ai.GetNextWaypoint(currentDestination, arrivalDistance, isOnPath);
     }
     void Update()
     {
@@ -52,7 +46,7 @@ public class BearMover : MonoBehaviour
         if (Vector3.Distance(transform.position, currentDestination.transform.position) <= arrivalDistance)
         {
             print("close");
-            currentDestination = GetNextWaypoint();
+            currentDestination = ai.GetNextWaypoint(currentDestination, arrivalDistance, isOnPath);
         }
     }
 
@@ -63,108 +57,10 @@ public class BearMover : MonoBehaviour
 
         if (Time.time - timeOfLastWaypointCheck > 1) //if more than a second has passed since Bear last looked for a waypoint
         {
-            currentDestination = GetNextWaypoint();
+            currentDestination = ai.GetNextWaypoint(currentDestination, arrivalDistance, isOnPath);
             timeOfLastWaypointCheck = Time.time;
         }
     }
 
-    private Waypoint GetNextWaypoint()
-    {
-        if(currentDestination == null || currentDestination.GetManualWaypoints() == null)
-        {
-            return AutoDetectNextWaypoint();
         }
-        else
-        {
-            return SelectFromManualWaypoints();
-        }
-    }
-
-    private Waypoint AutoDetectNextWaypoint()
-    {//finds the closest waypoint infront of Bear and returns it
-
-
-        //create a HashSet of waypoints which are within Bear's detection range
-        HashSet<Waypoint> detectibleWaypoints = new HashSet<Waypoint>();
-
-        foreach (Waypoint waypoint in waypoints)
-        {   //check vertical and total distnce first
-            if (Mathf.Abs(transform.position.y - waypoint.transform.position.y) < verticalLimit
-               && Vector3.Distance(transform.position, waypoint.transform.position) < visionDistance
-               && Vector3.Distance(transform.position, waypoint.transform.position) > arrivalDistance)
-            {
-                //create a direction vector between the Bear and the Waypoint
-                Vector3 waypointDir = waypoint.transform.position - transform.position;
-                //if the angle is less than required, add this Waypoint to the set
-                if (Vector3.Angle(transform.forward, waypointDir) < visionAngle)
-                {
-                    detectibleWaypoints.Add(waypoint);
-                }
-            }
-        }
-        if (detectibleWaypoints.Count == 0) //there are no waypoints in range
-        {
-            Debug.Log("There are no waypoints within detection range");
-            return null;
-        }
-        else if (detectibleWaypoints.Count == 1) //only one waypoint is in range, return it
-        {
-            foreach (Waypoint waypoint in detectibleWaypoints)
-            {
-                Debug.Log("There is one waypoint in range at position " + waypoint.transform.position);
-                return waypoint;
-            }
-            return null;
-        }
-        else //multiple waypoints, find which one is closest to Bear.
-        {
-            Waypoint closestWaypoint = null;
-            float closestDistance = 1000f;
-
-            foreach (Waypoint waypoint in detectibleWaypoints)
-            {
-                float distance = Vector3.Distance(transform.position, waypoint.transform.position);
-                if (distance < closestDistance)
-                {
-                    closestWaypoint = waypoint;
-                    closestDistance = distance;
-                }
-            }
-            Debug.Log("There are " + detectibleWaypoints.Count + " waypoints in range.");
-            return closestWaypoint;
-
-        }
-    }
-
-    private Waypoint SelectFromManualWaypoints()
-    {//finds which of the manually set waypoints is closest to Bear's forward
-
-        Waypoint[] manualWaypoints = currentDestination.GetManualWaypoints();
-
-        if(manualWaypoints.Length == 0)
-        {
-            return null;
-        }
-        else if(manualWaypoints.Length == 1)
-        {
-            return manualWaypoints[0];
-        }
-        else
-        {
-            Waypoint forwardestWaypoint = null;
-            float narrowestAngle = 360f;
-
-            foreach (Waypoint waypoint in manualWaypoints)
-            {
-                Vector3 targetDir = waypoint.transform.position - transform.position;
-                float targetAngle = Vector3.Angle(targetDir, transform.forward);
-                if (targetAngle < narrowestAngle)
-                {
-                    forwardestWaypoint = waypoint;
-                    narrowestAngle = targetAngle;
-                }
-            }
-            return forwardestWaypoint;
-        }
-    }
-}
+    
